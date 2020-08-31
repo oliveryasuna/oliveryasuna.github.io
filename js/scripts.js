1bbef5bba56b1fe163112c96c07d1bd177ecbf15
Oliver Yasuna <oliveryasuna.acct@gmail.com>
2020-08-31 16:18:09 -0400

    Recreated from scratch.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   $('a').click(function(e) {
     if($(this).attr('target') !== '_blank' && !e.ctrlKey) {
-      //$('#body-wrapper').addClass('blur');
+      $('#body-wrapper').addClass('blur');
       $('.loader-wrapper').removeClass('loader-wrapper-hidden');
     }
   });
