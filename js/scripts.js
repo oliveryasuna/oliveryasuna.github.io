3a51a545cb4ec3c7a1d0011f671f391e9ada5252
Oliver Yasuna <oliveryasuna.acct@gmail.com>
2020-09-16 17:58:19 -0400

    Big portfolio changes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
 
+  /*// Allow better <pre><code> formatting.
+  $("pre code").each(function() {
+    const $this = $(this);
+
+    const html = $this.html();
+    const pattern = html.match(/\s*\n[\t\s]*!/);
+
+    $this.html(html.replace(new RegExp(pattern, 'g'), '\n'));
+  });*/
+
   $('a').click(function(e) {
-    if($(this).attr('target') !== '_blank' && !e.ctrlKey) {
+    const $element = $(this);
+
+    if($element.attr('href') !== '#' && $element.attr('target') !== '_blank' && !e.ctrlKey) {
       $('#body-wrapper').addClass('blur');
       $('.loader-wrapper').removeClass('loader-wrapper-hidden');
     }
